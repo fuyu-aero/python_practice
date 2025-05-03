75af8e37b575f8ce5888ffd07646ddf05b1b3238
fuyu-aero <yuukettykitty@gmail.com>
2025-05-03 16:07:16 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Hello, Python with Anaconda!')
+plt.show()
